e8cf86e55002fd4f06311b8c7d405f21fc78e1d5
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-03-27 22:01:21 +0400

    draft Dialogs layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import cls from "./Dialogs.module.css";
 
 const Dialogs = props => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={cls.dialogs}>
+      <div className={cls.dialogItems}>
+        <div className={cls.dialog + " " + cls.active}>Jorica</div>
+        <div className={cls.dialog}>Petrica</div>
+        <div className={cls.dialog}>Volodea</div>
+        <div className={cls.dialog}>Anton</div>
+        <div className={cls.dialog}>Cocos</div>
+      </div>
+      <div className={cls.messages}>
+        <div className={cls.message}>Hi</div>
+        <div className={cls.message}>How are you?</div>
+        <div className={cls.message}>Fine</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
