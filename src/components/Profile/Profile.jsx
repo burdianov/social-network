c0eff034dde72368a93e3dacdf63ad32436d5113
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-03-29 21:50:19 +0400

    minor refactoring

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cls from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
-import ProfileInfo from "./MyPosts/ProfileInfo/ProfileInfo";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
   return (
