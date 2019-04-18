34197efc11aa42a205362c0204280148222c5064
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-04-18 23:00:45 +0400

    minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
